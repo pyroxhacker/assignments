1c6f743fd4ef29d6c8c8916eefbff67bd5e744c6
pyroxhacker <rjana9580@gmail.com>
2025-08-06 23:24:13 +0530

    Initial commit

--- python/assignment2.py
+++ python/assignment2.py
@@ -1,1 +1,19 @@
-print("hello world")
+def findMax(arr, i):
+  
+    # Last index returns the element
+    if i == len(arr) - 1:
+        return arr[i]
+
+    # Find the maximum from the rest of the list
+    recMax = findMax(arr, i + 1)
+
+    # Compare with i-th element and return
+    return max(recMax, arr[i])
+
+
+def largest(arr):
+    return findMax(arr, 0)
+
+if __name__ == '__main__':
+  arr = [10, 324, 45, 90, 9808]
+  print(largest(arr))
